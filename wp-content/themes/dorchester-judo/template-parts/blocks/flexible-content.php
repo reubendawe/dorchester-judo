f7489a8fac7045a6897ef1c44274f10839b82207
Reuben Dawe <reubendawemarketing@gmail.com>
2026-01-12 19:50:39 +0000

    PHP dynamic code pulled through for hero title

--- wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
+++ wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
@@ -1,8 +1,23 @@
 <!-- Hero image -->
 
+<?php if ( have_rows('flexible_content')) : ?>
+    <?php while ( have_rows('flexible_content') ) : the_row(); ?>
+        <?php if ( get_row_layout() == 'hero_section') : ?>
+            <?php
+            $image = get_sub_field('image');
+            $title = get_sub_field('title');
+            ?>
+        <?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+
 <section class="hero-section">
    <div class="px-4 py-5 my-5 text-center">
-    <h1 class="display-5 fw-bold"><?php the_title(); ?> Section</h1>
+
+   <?php if ( $title ) : ?>
+    <h1 class="display-5 fw-bold"><?php echo wp_kses_post($title); ?></h1>
+   <?php endif; ?>
+
     <div class="col-lg-6 mx-auto">
         <p class="Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."></p>
     </div>
