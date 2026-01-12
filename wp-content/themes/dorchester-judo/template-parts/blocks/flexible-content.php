bb8d198318ebc8128d326cd7ed25e7352f1335d8
Reuben Dawe <reubendawemarketing@gmail.com>
2026-01-12 20:34:36 +0000

    Success with hero image & pulling in ACF image and title

--- wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
+++ wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
@@ -13,15 +13,15 @@
 
 <section class="hero-section">
    <div class="px-4 py-5 my-5 text-center">
+    <?php if ( $image ) : ?>
+       <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="hero-image">
+    <?php endif; ?>
 
    <?php if ( $title ) : ?>
     <h1 class="display-5 fw-bold"><?php echo wp_kses_post($title); ?></h1>
    <?php endif; ?>
 
-    <div class="col-lg-6 mx-auto">
-        <p class="Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."></p>
-    </div>
-    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <div class="d-grid gap-2 d-sm-flex pt-3 justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
         <button type="button" class="btn btn-outline-secondary btn-lg px-4">Primary button</button>
     </div>
