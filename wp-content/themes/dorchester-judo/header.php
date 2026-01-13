99a08f7c5034209d76867a32df98acfd7d02c54d
Reuben <reubendawemarketing@gmail.com>
2026-01-13 10:46:06 +0000

    Boilerplate header added - Alignment needs fixing, links need adding, etc.

--- wp-content/themes/dorchester-judo/header.php
+++ wp-content/themes/dorchester-judo/header.php
@@ -13,10 +13,36 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
+<div class="bg-dark sticky-top">
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3">
+            <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4 text-white">Simple header</span>
+            </a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" aria-current="page">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" aria-current="page">Classes</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" aria-current="page">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" aria-current="page">Contact</a>
+                </li>
+            </ul>
+        </header>
+    </div>
+</div>
--- wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
+++ wp-content/themes/dorchester-judo/template-parts/blocks/flexible-content.php
@@ -1,8 +1,8 @@
 <!-- Hero image -->
 
-<?php if ( have_rows('flexible_content')) : ?>
-    <?php while ( have_rows('flexible_content') ) : the_row(); ?>
-        <?php if ( get_row_layout() == 'hero_section') : ?>
+<?php if (have_rows('flexible_content')) : ?>
+    <?php while (have_rows('flexible_content')) : the_row(); ?>
+        <?php if (get_row_layout() == 'hero_section') : ?>
             <?php
             $image = get_sub_field('image');
             $title = get_sub_field('title');
@@ -12,18 +12,18 @@
 <?php endif; ?>
 
 <section class="hero-section">
-   <div class="px-4 py-5 my-5 text-center">
-    <?php if ( $image ) : ?>
-       <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="hero-image">
-    <?php endif; ?>
+    <div class="px-4 py-5 my-5 text-center">
+        <?php if ($image) : ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="hero-image">
+        <?php endif; ?>
 
-   <?php if ( $title ) : ?>
-    <h1 class="display-5 fw-bold"><?php echo wp_kses_post($title); ?></h1>
-   <?php endif; ?>
+        <?php if ($title) : ?>
+            <h1 class="display-5 fw-bold"><?php echo wp_kses_post($title); ?></h1>
+        <?php endif; ?>
 
-    <div class="d-grid gap-2 d-sm-flex pt-3 justify-content-sm-center">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Primary button</button>
+        <div class="d-grid gap-2 d-sm-flex pt-3 justify-content-sm-center">
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Primary button</button>
+        </div>
     </div>
-   </div>
 </section>
